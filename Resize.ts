f1435253a8747c246389acf9263ab43b61037277
UttamRahane <uttamrahane@gmail.com>
2020-03-27 15:18:27 +0530

    Added resize directive

---
+++ Resize.ts
@@ -0,0 +1,17 @@
+import { DirectiveOptions } from 'vue';
+
+const directive: DirectiveOptions = {
+    inserted(el, binding) {
+        const callback = binding.value;
+        let timeout = null;
+        const resizeObserver = new ResizeObserver(() => {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                callback();
+            }, 500);
+        });
+        resizeObserver.observe(el);
+    },
+};
+
+export default directive;
